90c93a3b92745fa2ec801dd69586f1754ff4df51
wasp97 <imirc97@gmail.com>
2021-12-07 18:05:50 +0100

    removed sudo from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 /usr/bin/git pull
-sudo apt-get install python3.8 -y
-sudo apt install python3-pip -y
+apt-get install python3.8 -y
+apt install python3-pip -y
 pip3 install -f requirements.txt
 python3 greenhouse/Dashboard/daschboard_grafana.py
